4b14e0bc762fd03ce45d406e160c271fc1e2ac65
Alimonta Axel Ian <axelalimonta@gmail.com>
2022-06-11 23:02:05 -0300

    agregado DFS

--- busquedaPorProfundidadDFS/src/busquedaPorProfundidadDFS/Grafo.java
+++ busquedaPorProfundidadDFS/src/busquedaPorProfundidadDFS/Grafo.java
@@ -1,10 +1,15 @@
 package busquedaPorProfundidadDFS;
 
+import java.util.Stack;
+
 public class Grafo {
 	private int [][]matrizDeAdyacencia;
+	private Stack<Integer>pila=new Stack<Integer>();
+	boolean[]visitados;
 	
 	public Grafo(int cantNodos) {
-		matrizDeAdyacencia=new int[cantNodos][cantNodos];
+		matrizDeAdyacencia=new int[cantNodos+1][cantNodos+1];
+		visitados=new boolean[cantNodos+1];
 	}
 	
 	public void setArista(int nodo1,int nodo2) {
@@ -12,4 +17,22 @@ public class Grafo {
 		matrizDeAdyacencia[nodo2][nodo1]=1;
 	}
 	
+	public void recorrerGrafo(int nodo) {
+		visitados[nodo]=true;
+		pila.push(nodo);
+		while(!pila.empty()) {
+			nodo=pila.pop();
+			mostrarNodo(nodo);
+			for(int i=1;i<matrizDeAdyacencia.length;i++) {
+				if(matrizDeAdyacencia[nodo][i]==1&&visitados[i]==false) {
+					visitados[i]=true;
+					pila.push(i);
+				}				
+			}
+		}
+	}
+	
+	private void mostrarNodo(int nodo) {
+		System.out.print(nodo+" ");
+	}
 }
