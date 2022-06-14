c46d6eb9e7ffc9284c34e78a219d729e3c0ef5f4
Alimonta Axel Ian <axelalimonta@gmail.com>
2022-06-14 03:55:18 -0300

    Prim_matAdy

---
+++ Prim_matAdy/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		Grafo grafo=new Grafo(4);
+		ArrayList<Integer>listaVerticesConectados=new ArrayList<Integer>();
+		listaVerticesConectados.add(1);
+		listaVerticesConectados.add(4);
+		
+		grafo.setArista(new Arista(1,2,2));
+		grafo.setArista(new Arista(1,3,4));
+		grafo.setArista(new Arista(1,4,4));
+		grafo.setArista(new Arista(3,2,3));
+		grafo.setArista(new Arista(2,4,2));
+		grafo.setArista(new Arista(3,4,1));
+		
+		System.out.println(grafo.prim(listaVerticesConectados));
+	}
+}
